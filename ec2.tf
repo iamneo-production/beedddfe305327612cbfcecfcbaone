7801d5fbd2fc3beadd3339140c34dbe24c3fc4e2
Iamneo <techadmin@iamneo.ai>
2023-09-22 14:46:42 +0000

    Backup Commit

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,14 @@
-provider "aws"{
-    region=""
+terraform{
+    required_providers{
+        aws={
+            source="hashicorp/aws"
+            version="5.17.0"
+        }
+    }
+}
+    
+    provider "aws"{
+    region="ap-northeast-1"
     access_key="AKIAVLQIRAIXBG6LRFGI"
     secret_key="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"
 }
@@ -8,4 +17,6 @@ resource "aws_instance" "ubuntu"{
     ami=""
     instance_type="t2.micro"
 }
-output "public_ip"
+output "public_ip"{
+    value=aws_instance.example.public_ip
+}
